2a74fc02108c6f4421a58064644cf44a57054f9a
Jason Ma <z5161364@student.unsw.edu.au>
2020-02-07 08:52:25 +1100

    Project Creation (plus some dependencies?)

---
+++ app/src/main/java/com/example/yetanotherjnovelreader/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.yetanotherjnovelreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
