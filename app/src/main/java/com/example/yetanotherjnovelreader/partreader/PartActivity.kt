819cef7ba43097379cbb282bdce20d857913c371
Jason Ma <z5161364@student.unsw.edu.au>
2020-02-14 20:00:53 +1100

    Loading screen added to part reader

--- app/src/main/java/com/example/yetanotherjnovelreader/partreader/PartActivity.kt
+++ app/src/main/java/com/example/yetanotherjnovelreader/partreader/PartActivity.kt
@@ -1,11 +1,15 @@
 package com.example.yetanotherjnovelreader.partreader
 
+import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.addListener
 import androidx.lifecycle.observe
 import com.example.yetanotherjnovelreader.R
 import com.example.yetanotherjnovelreader.data.Repository
@@ -24,13 +28,17 @@ class PartActivity : AppCompatActivity() {
         )
     }
 
+    private lateinit var loadBar: ProgressBar
+    private lateinit var scrollView: ScrollView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_part)
 
         partId = intent.getStringExtra(EXTRA_PART_ID)
 
-        val scrollView = findViewById<ScrollView>(R.id.content_scroll_container)
+        loadBar = findViewById(R.id.load_bar)
+        scrollView = findViewById(R.id.content_scroll_container)
         val textView = findViewById<TextView>(R.id.content_view)
 
         val mainTextView = findViewById<TextView>(R.id.content_view)
@@ -43,7 +51,8 @@ class PartActivity : AppCompatActivity() {
         // TODO: Add indicator that part is still loading until below observer is fired
         viewModel.initialPartProgress.observe(this) { percentage ->
             val position = textView.height * percentage
-            scrollView.smoothScrollTo(0, position.toInt())
+            scrollView.scrollTo(0, position.toInt())
+            transitionToContent()
         }
 
         scrollView.setOnScrollChangeListener { _, _, scrollY, _, _ ->
@@ -56,5 +65,17 @@ class PartActivity : AppCompatActivity() {
                 1.0
             }
         }
+    }
+
+    private fun transitionToContent() {
+        val loadBarAnimator = AnimatorInflater.loadAnimator(this, android.R.animator.fade_out)
+        loadBarAnimator.setTarget(loadBar)
+        loadBarAnimator.addListener(onEnd = { loadBar.visibility = View.GONE })
+        loadBarAnimator.start()
+
+        val scrollViewAnimator = AnimatorInflater.loadAnimator(this, R.animator.slide_from_bottom)
+        scrollViewAnimator.setTarget(scrollView)
+        scrollViewAnimator.addListener(onStart = { scrollView.visibility = View.VISIBLE })
+        scrollViewAnimator.start()
     }
 }
