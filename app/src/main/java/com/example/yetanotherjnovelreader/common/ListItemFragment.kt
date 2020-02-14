fcd71fdb37b219e3d6b09f866ad784beccd68b95
Jason Ma <z5161364@student.unsw.edu.au>
2020-02-15 09:30:55 +1100

    Loading screen added to ListItemFragment

--- app/src/main/java/com/example/yetanotherjnovelreader/common/ListItemFragment.kt
+++ app/src/main/java/com/example/yetanotherjnovelreader/common/ListItemFragment.kt
@@ -1,9 +1,12 @@
 package com.example.yetanotherjnovelreader.common
 
+import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.core.animation.addListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
@@ -25,12 +28,16 @@ class ListItemFragment : Fragment(), ListItem.InteractionListener {
     private var uid = 0
     private var recyclerViewAdapter = CustomRecyclerViewAdapter(this)
 
+    private lateinit var loadBar: ProgressBar
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         uid = requireArguments().getInt(ARG_ID, 0)
         viewModel.getItemList(uid).observe(this) {
             recyclerViewAdapter.setItems(it)
+            transitionToContent()
         }
     }
 
@@ -38,6 +45,11 @@ class ListItemFragment : Fragment(), ListItem.InteractionListener {
         super.onResume()
         // Force redraw for potentially updated recycler_item progress values
         recyclerViewAdapter.notifyDataSetChanged()
+
+        if (viewModel.getItemList(uid).value?.isNotEmpty() == true) {
+            loadBar.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+        }
     }
 
     override fun onCreateView(
@@ -45,8 +57,9 @@ class ListItemFragment : Fragment(), ListItem.InteractionListener {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_listitem_list, container, false)
-
-        if (view is RecyclerView) with (view) {
+        loadBar = view.findViewById(R.id.load_bar)
+        recyclerView = view.findViewById(R.id.list)
+        with (recyclerView) {
             layoutManager = LinearLayoutManager(context)
             adapter = recyclerViewAdapter
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
@@ -56,5 +69,17 @@ class ListItemFragment : Fragment(), ListItem.InteractionListener {
 
     override fun onClick(item: ListItem) {
         viewModel.listItemFragmentViewOnClick(uid, item)
+    }
+
+    private fun transitionToContent() {
+        val loadBarAnimator = AnimatorInflater.loadAnimator(context, android.R.animator.fade_out)
+        loadBarAnimator.setTarget(loadBar)
+        loadBarAnimator.addListener(onEnd = { loadBar.visibility = View.GONE })
+        loadBarAnimator.start()
+
+        val scrollViewAnimator = AnimatorInflater.loadAnimator(context, R.animator.slide_from_right)
+        scrollViewAnimator.setTarget(recyclerView)
+        scrollViewAnimator.addListener(onStart = { recyclerView.visibility = View.VISIBLE })
+        scrollViewAnimator.start()
     }
 }
