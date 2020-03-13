118501f1c8f455537db81a46452269b79859f229
Jason Ma <ytrewqwert@gmail.com>
2020-03-13 11:38:21 +1100

    PaginatedReader: Correctly set margins and text size/style.

--- app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/partreader/pagedreader/PagedReaderPageFragment.kt
+++ app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/partreader/pagedreader/PagedReaderPageFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.ytrewqwert.yetanotherjnovelreader.R
+import com.ytrewqwert.yetanotherjnovelreader.databinding.FragmentPagedReaderPageBinding
+import com.ytrewqwert.yetanotherjnovelreader.partreader.PartViewModel
 
 class PagedReaderPageFragment : Fragment() {
     companion object {
@@ -14,13 +18,20 @@ class PagedReaderPageFragment : Fragment() {
     }
 
     private var textView: TextView? = null
+    private val viewModel by activityViewModels<PartViewModel>()
+    private var binding: FragmentPagedReaderPageBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_paged_reader_page, container, false)
-        textView = view.findViewById(R.id.page_contents)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_paged_reader_page, container, false
+        )
+        binding?.lifecycleOwner = viewLifecycleOwner
+        binding?.viewModel = viewModel
+        val view = binding?.root
+        textView = view?.findViewById(R.id.page_contents)
 
         val text = requireArguments().getCharSequence(ARG_PAGE_CONTENT) ?: "Page content not given"
         textView?.text = text
