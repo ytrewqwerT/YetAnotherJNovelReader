a8f0e69954ee8aa56a1764b2e256f75ef9c596e5
Jason Ma <ytrewqwert@gmail.com>
2020-07-06 14:58:36 +1000

    Replace font "Crimson Text" with "Amiri".
    Although nice, Crimson Text had inherent line-spacing issues within the family...

--- app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/local/preferences/FontResIds.kt
+++ app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/local/preferences/FontResIds.kt
@@ -16,7 +16,7 @@ object FontResIds {
     fun getFontResourceId(fontStr: String): Int? = when (fontStr) {
         "alegreya" -> R.font.alegreya_family
         "alegreya_sans" -> R.font.alegreya_sans_family
-        "crimson_text" -> R.font.crimson_text_family
+        "amiri" -> R.font.amiri_family
         "lora" -> R.font.lora_family
         "open_sans" -> R.font.open_sans_family
         else -> null
