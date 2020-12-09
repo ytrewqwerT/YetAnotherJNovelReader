9fdea0f880d36db59735e494a9d5f2af9b60d685
Jason Ma <ytrewqwert@gmail.com>
2020-12-10 08:51:16 +1100

    Implement paragraph spacing preference

--- app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/htmlparser/tags/pairtags/PPairTagApplier.kt
+++ app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/htmlparser/tags/pairtags/PPairTagApplier.kt
@@ -1,7 +1,9 @@
 package com.ytrewqwert.yetanotherjnovelreader.data.htmlparser.tags.pairtags
 
+import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.LeadingMarginSpan
+import android.text.style.RelativeSizeSpan
 import com.ytrewqwert.yetanotherjnovelreader.data.Repository
 import com.ytrewqwert.yetanotherjnovelreader.data.htmlparser.tags.PairTagApplier
 import com.ytrewqwert.yetanotherjnovelreader.data.local.preferences.PrefDefaults
@@ -17,9 +19,19 @@ class PPairTagApplier(partId: CharSequence) : PairTagApplier(partId) {
     override fun apply(
         args: List<Pair<CharSequence, CharSequence>>, contents: SpannableStringBuilder
     ) {
-        val indentation = Repository.getInstance()?.getReaderSpanSettings()?.paraIndent ?: PrefDefaults.PARA_INDENT
+        val readerSpanSettings = Repository.getInstance()?.getReaderSpanSettings()
+
+        val indentation = readerSpanSettings?.paraIndent ?: PrefDefaults.PARA_INDENT
         applySpans(contents, LeadingMarginSpan.Standard(indentation, 0)) // Paragraph indentation
         applyArgs(contents, args)
-        contents.append("\n")
+        contents.append("\n\n") // Extra '\n' for applying paragraph spacing
+
+        val paragraphSpacing = (readerSpanSettings?.paraSpacing ?: PrefDefaults.PARA_SPACING) - 1
+        contents.setSpan(
+            RelativeSizeSpan(paragraphSpacing),
+            contents.length-1,
+            contents.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 }
