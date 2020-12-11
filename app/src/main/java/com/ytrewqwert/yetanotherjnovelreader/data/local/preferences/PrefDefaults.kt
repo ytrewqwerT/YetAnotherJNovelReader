d8e4a5a2fcc4a6904787a569731de91e87e1e8b4
Jason Ma <ytrewqwert@gmail.com>
2020-12-11 12:16:04 +1100

    Unify preference identifiers between the settings screen and internal retrieval to use the same source.

--- app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/local/preferences/PrefDefaults.kt
+++ app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/data/local/preferences/PrefDefaults.kt
@@ -1,11 +1,14 @@
 package com.ytrewqwert.yetanotherjnovelreader.data.local.preferences
 
+import com.ytrewqwert.yetanotherjnovelreader.App
+import com.ytrewqwert.yetanotherjnovelreader.R
+
 /** Defines default values for specific preferences. */
 object PrefDefaults {
-    const val IS_HORIZONTAL = false
-    const val FONT_SIZE = 15
-    const val MARGIN = 16
-    const val LINE_SPACING = 1.0f
-    const val PARA_SPACING = 0.0f
-    const val PARA_INDENT = 100
+    val IS_HORIZONTAL = App.getBool(R.bool.d_horizontal_reader)
+    val FONT_SIZE = App.getInt(R.integer.d_font_size)
+    val MARGIN = App.getInt(R.integer.d_margin)
+    val LINE_SPACING = App.getFloat(R.dimen.d_line_spacing)
+    val PARA_SPACING = App.getFloat(R.dimen.d_para_spacing)
+    val PARA_INDENT = App.getInt(R.integer.d_para_indent)
 }
