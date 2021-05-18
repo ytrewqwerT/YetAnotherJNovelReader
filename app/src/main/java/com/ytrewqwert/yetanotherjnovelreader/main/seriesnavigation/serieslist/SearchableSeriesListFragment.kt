501d9ccc6614b2320fd5d2190da0e88087c59db0
Jason Ma <ytrewqwert@gmail.com>
2021-05-18 11:13:15 +1000

    Series list's search bar can now be hidden/shown

--- app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/main/seriesnavigation/serieslist/SearchableSeriesListFragment.kt
+++ app/src/main/java/com/ytrewqwert/yetanotherjnovelreader/main/seriesnavigation/serieslist/SearchableSeriesListFragment.kt
@@ -1,9 +1,9 @@
 package com.ytrewqwert.yetanotherjnovelreader.main.seriesnavigation.serieslist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
@@ -15,6 +15,13 @@ import com.ytrewqwert.yetanotherjnovelreader.databinding.FragmentSearchableListB
 class SearchableSeriesListFragment : Fragment() {
     private val viewModel by viewModels<SeriesListViewModel> {
         RepositoriedViewModelFactory(Repository.getInstance(requireContext()))
+    }
+
+    private var searchBarVisible = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -29,5 +36,32 @@ class SearchableSeriesListFragment : Fragment() {
             replace(R.id.list_container, SeriesListFragment::class.java, null, "series_list_fragment")
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // Piggyback off of the existing menu (MainActivity's) rather than create our own...
+        // Show the (by default hidden) search icon in the menu (if it exists...)
+        val searchButton = menu.findItem(R.id.search) ?: return
+        searchButton.isVisible = true
+        searchButton.setOnMenuItemClickListener {
+            (view as? MotionLayout)?.let {
+                val searchView = it.findViewById<View>(R.id.search_field)
+                val imm = requireContext().getSystemService(InputMethodManager::class.java)
+                when (searchBarVisible) {
+                    true  -> {
+                        it.transitionToStart()
+                        searchView.clearFocus()
+                        imm.hideSoftInputFromWindow(searchView.windowToken, 0)
+                    }
+                    false -> {
+                        it.transitionToEnd()
+                        searchView.requestFocus()
+                        imm.showSoftInput(searchView, 0)
+                    }
+                }
+                searchBarVisible = !searchBarVisible
+            }
+            true
+        }
     }
 }
